98ac5fdce23497ee4e6814749ca47ca573a7d91b
midoelmelegy <midoelmelegy@gmail.com>
2021-07-01 15:29:55 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,19 +3,19 @@ module.exports = {
     "description": "Yearn City Electric Car pays 60 YRN tokens weekly to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1000.png",
     "image": "https://yearncity.herokuapp.com/images/1000.png",
-    "name": "Electric Car "
+    "name": "Electric Car"
   },
   "1001": {
     "description": "Yearn City Sedan Car pays 135 YRN tokens weekly to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1001.png",
     "image": "https://yearncity.herokuapp.com/images/1001.png",
-    "name": "Sedan Car "
+    "name": "Sedan Car"
   },
   "1002": {
     "description": "Yearn City Sports Car pays 375 YRN tokens weekly to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1002.png",
     "image": "https://yearncity.herokuapp.com/images/1002.png",
-    "name": "Sports Car "
+    "name": "Sports Car"
   },
   "1003": {
     "description": "Yearn City Big Van pays 550 YRN tokens weekly to NFT holder.",
@@ -27,6 +27,6 @@ module.exports = {
     "description": "Yearn City Big Buss pays 937.5 YRN tokens weekly to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1004.png",
     "image": "https://yearncity.herokuapp.com/images/1004.png",
-    "name": "Big Buss "
+    "name": "Big Buss"
   }
 }
