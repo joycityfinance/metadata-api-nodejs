3da4520aee07952fb86f7e049b5bd5177d2688c9
midoelmelegy <midoelmelegy@gmail.com>
2021-07-01 10:51:28 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -4,22 +4,22 @@ module.exports = {
     "description": "This Yearn City Electric Car pays 60 Earn tokens every week to NFT holder.",
     "birthday": "10-18"
   },
-  "2": {
+  "1002": {
     "name": "Sedan Car",
     "description": "This Yearn City Sedan Car pays 135 EARN tokens every week to NFT holder.",
     "birthday": "01-08"
   },
-  "3": {
+  "1017": {
     "name": "Sports Car",
     "description": "This Yearn City Sports Car pays 375 EARN tokens every week to NFT holder.",
     "birthday": "01-22"
   },
-  "4": {
+  "1004": {
     "name": "Big Van",
     "description": "This Yearn City Big Van pays 550 EARN tokens every week to NFT holder.",
     "birthday": "03-04"
   },
-  "5": {
+  "1005": {
     "name": "Big Buss",
     "description": "This Yearn City Big Buss pays 937.5 EARN tokens every week to NFT holder.",
     "birthday": "03-09"
