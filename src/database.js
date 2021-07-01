be3a1228f4248b736e5592d51b24ec426b60d32d
midoelmelegy <midoelmelegy@gmail.com>
2021-07-01 15:15:36 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,32 +1,32 @@
 module.exports = {
-  "1001": {
+  "1000": {
     "description": "Yearn City Electric Car pays 60 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1001.png",
-    "image": "https://yearncity.herokuapp.com/images/1001.png",
+    "external_url": "https://yearncity.herokuapp.com/images/1000.png",
+    "image": "https://yearncity.herokuapp.com/images/1000.png",
     "name": "Electric Car "
   },
-  "1002": {
+  "1001": {
     "description": "Yearn City Sedan Car pays 135 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1002.png",
-    "image": "https://yearncity.herokuapp.com/images/1002.png",
+    "external_url": "https://yearncity.herokuapp.com/images/1001.png",
+    "image": "https://yearncity.herokuapp.com/images/1001.png",
     "name": "Sedan Car "
   },
-  "1003": {
+  "1002": {
     "description": "Yearn City Sports Car pays 375 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1003.png",
-    "image": "https://yearncity.herokuapp.com/images/1003.png",
+    "external_url": "https://yearncity.herokuapp.com/images/1002.png",
+    "image": "https://yearncity.herokuapp.com/images/1002.png",
     "name": "Sports Car "
   },
-  "1004": {
+  "1003": {
     "description": "Yearn City Big Van pays 550 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1004.png",
-    "image": "https://yearncity.herokuapp.com/images/1004.png",
+    "external_url": "https://yearncity.herokuapp.com/images/1003.png",
+    "image": "https://yearncity.herokuapp.com/images/1003.png",
     "name": "Big Van"
   },
-  "1005": {
+  "1004": {
     "description": "Yearn City Big Buss pays 937.5 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1005.png",
-    "image": "https://yearncity.herokuapp.com/images/1005.png",
+    "external_url": "https://yearncity.herokuapp.com/images/1004.png",
+    "image": "https://yearncity.herokuapp.com/images/1004.png",
     "name": "Big Buss "
   }
 }
