483c4981a6d4906ac89606aa29e693f12eb8dfa0
midoelmelegy <midoelmelegy@gmail.com>
2021-07-01 11:46:12 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,31 +3,31 @@ module.exports = {
     "description": "This Yearn City Electric Car pays 60 Earn tokens every week to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1001.png",
     "image": "https://yearncity.herokuapp.com/images/1001.png",
-    "name": "Electric Car",
+    "name": "Electric Car"
   },
   "1002": {
     "description": "This Yearn City Sedan Car pays 135 EARN tokens every week to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1001.png",
     "image": "https://yearncity.herokuapp.com/images/1001.png",
-    "name": "Sedan Car",
+    "name": "Sedan Car"
   },
   "1017": {
     "description": "This Yearn City Sports Car pays 375 EARN tokens every week to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1017.png",
     "image": "https://yearncity.herokuapp.com/images/1017.png",
-    "name": "Sports Car",
+    "name": "Sports Car"
   },
   "1003": {
     "description": "This Yearn City Big Van pays 550 EARN tokens every week to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1003.png",
     "image": "https://yearncity.herokuapp.com/images/1003.png",
-    "name": "Big Van",
+    "name": "Big Van"
   },
   "1004": {
     "description": "This Yearn City Big Buss pays 937.5 EARN tokens every week to NFT holder.",
     "external_url": "https://yearncity.herokuapp.com/images/1004.png",
     "image": "https://yearncity.herokuapp.com/images/1004.png",
     "name": "Big Buss",
-    "description": "This Yearn City Big Buss pays 937.5 EARN tokens every week to NFT holder.",
+    "description": "This Yearn City Big Buss pays 937.5 EARN tokens every week to NFT holder."
   }
 }
