27984f647319b217c093f3b238d1572ebafb4795
midoelmelegy <midoelmelegy@gmail.com>
2021-07-10 00:03:45 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,92 +1,92 @@
 module.exports = {
   "1000": {
-    "description": "Yearn City Electric Car pays 60 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1000.png",
-    "image": "https://yearncity.herokuapp.com/images/1000.png",
-    "name": "Electric Car"
+    "description": "Joy City Ice Cream Mini pays 60 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1000.png",
+    "image": "https://joycity.herokuapp.com/images/1000.png",
+    "name": "Ice Cream Mini"
   },
   "1001": {
-    "description": "Yearn City Sedan Car pays 135 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1001.png",
-    "image": "https://yearncity.herokuapp.com/images/1001.png",
-    "name": "Sedan Car"
+    "description": "Joy City Grand Jeep pays 135 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1001.png",
+    "image": "https://joycity.herokuapp.com/images/1001.png",
+    "name": "Grand Jeep"
   },
   "1002": {
-    "description": "Yearn City Sports Car pays 375 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1002.png",
-    "image": "https://yearncity.herokuapp.com/images/1002.png",
-    "name": "Sports Car"
+    "description": "Joy City Blue Mini pays 375 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1002.png",
+    "image": "https://joycity.herokuapp.com/images/1002.png",
+    "name": "Blue Mini"
   },
   "1003": {
-    "description": "Yearn City Big Van pays 550 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1003.png",
-    "image": "https://yearncity.herokuapp.com/images/1003.png",
-    "name": "Big Van"
+    "description": "Joy City Blue Rally pays 550 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1003.png",
+    "image": "https://joycity.herokuapp.com/images/1003.png",
+    "name": "Blue Rally"
   },
   "1004": {
-    "description": "Yearn City Big Buss pays 937.5 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1004.png",
-    "image": "https://yearncity.herokuapp.com/images/1004.png",
-    "name": "Big Buss"
+    "description": "Joy City Black Sports pays 937.5 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1004.png",
+    "image": "https://joycity.herokuapp.com/images/1004.png",
+    "name": "Black Sports"
   },
   "1005": {
-    "description": "Yearn City Petrol Station pays 8,250 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1005.png",
-    "image": "https://yearncity.herokuapp.com/images/1005.png",
-    "name": "Petrol Station"
+    "description": "Joy City Gas Pump pays 8,250 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1005.png",
+    "image": "https://joycity.herokuapp.com/images/1005.png",
+    "name": "Gas Pump"
   },
   "1007": {
-    "description": "Yearn City Car Wash pays 3,000 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1006.png",
-    "image": "https://yearncity.herokuapp.com/images/1006.png",
-    "name": "Car Wash"
+    "description": "Joy City Ride Wash pays 3,000 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1006.png",
+    "image": "https://joycity.herokuapp.com/images/1006.png",
+    "name": "Ride Wash"
   },
   "1015": {
-    "description": "Yearn City Car Rental pays 2,100 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1007.png",
-    "image": "https://yearncity.herokuapp.com/images/1007.png",
-    "name": "Car Rental"
+    "description": "Joy City Ride Rental pays 2,100 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1007.png",
+    "image": "https://joycity.herokuapp.com/images/1007.png",
+    "name": "Ride Rental"
   },
   "1016": {
-    "description": "Yearn City Car Repaire pays 3,750 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1008.png",
-    "image": "https://yearncity.herokuapp.com/images/1008.png",
-    "name": "Car Repaire"
+    "description": "Joy City Mechanic pays 3,750 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1008.png",
+    "image": "https://joycity.herokuapp.com/images/1008.png",
+    "name": "Mechanic"
   },
   "1006": {
-    "description": "Yearn City Power Station pays 6,500 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1009.png",
-    "image": "https://yearncity.herokuapp.com/images/1009.png",
+    "description": "Joy City Power Station pays 6,500 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1009.png",
+    "image": "https://joycity.herokuapp.com/images/1009.png",
     "name": "Power Station"
   },
   "1008": {
-    "description": "Yearn City Cinema pays 10,800 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1010.png",
-    "image": "https://yearncity.herokuapp.com/images/1010.png",
+    "description": "Joy City Cinema pays 10,800 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1010.png",
+    "image": "https://joycity.herokuapp.com/images/1010.png",
     "name": "Cinema"
   },
   "1011": {
-    "description": "Yearn City Restaurant pays 5,225 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1011.png",
-    "image": "https://yearncity.herokuapp.com/images/1011.png",
+    "description": "Joy City Restaurant pays 5,225 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1011.png",
+    "image": "https://joycity.herokuapp.com/images/1011.png",
     "name": "Restaurant"
   },
   "1012": {
-    "description": "Yearn City Hotel pays 13,125 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1012.png",
-    "image": "https://yearncity.herokuapp.com/images/1012.png",
-    "name": "Hotel"
+    "description": "Joy City Motel pays 13,125 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1012.png",
+    "image": "https://joycity.herokuapp.com/images/1012.png",
+    "name": "Motel"
   },
   "1013": {
-    "description": "Yearn City Supermarket pays 4,500 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1013.png",
-    "image": "https://yearncity.herokuapp.com/images/1013.png",
+    "description": "Joy City Supermarket pays 4,500 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1013.png",
+    "image": "https://joycity.herokuapp.com/images/1013.png",
     "name": "Supermarket"
   },
   "1014": {
-    "description": "Yearn City Coffee Shop pays 1,500 YRN tokens weekly to NFT holder.",
-    "external_url": "https://yearncity.herokuapp.com/images/1014.png",
-    "image": "https://yearncity.herokuapp.com/images/1014.png",
-    "name": "Coffee Shop"
+    "description": "Joy City Coffee House pays 1,500 JOY tokens weekly to NFT holder.",
+    "external_url": "https://joycity.herokuapp.com/images/1014.png",
+    "image": "https://joycity.herokuapp.com/images/1014.png",
+    "name": "Coffee House"
   }
 }
