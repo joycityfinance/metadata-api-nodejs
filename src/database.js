301f0fa1cfec7d1f34bf51dd00e528677546e79a
midoelmelegy <midoelmelegy@gmail.com>
2021-11-30 19:37:02 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,89 +1,89 @@
 module.exports = {
-  "1000": {
+  "1": {
     "description": "Joy City Ice Cream Mini pays 60 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1000.png",
     "image": "https://joycity.herokuapp.com/images/1000.png",
     "name": "Ice Cream Mini"
   },
-  "1001": {
+  "2": {
     "description": "Joy City Grand Jeep pays 135 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1001.png",
     "image": "https://joycity.herokuapp.com/images/1001.png",
     "name": "Grand Jeep"
   },
-  "1002": {
+  "3": {
     "description": "Joy City Mini Coop pays 375 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1002.png",
     "image": "https://joycity.herokuapp.com/images/1002.png",
     "name": "Mini Coop"
   },
-  "1003": {
+  "4": {
     "description": "Joy City Blue Rally pays 550 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1003.png",
     "image": "https://joycity.herokuapp.com/images/1003.png",
     "name": "Blue Rally"
   },
-  "1004": {
+  "5": {
     "description": "Joy City Black Sports pays 937.5 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1004.png",
     "image": "https://joycity.herokuapp.com/images/1004.png",
     "name": "Black Sports"
   },
-  "1005": {
+  "6": {
     "description": "Joy City Gas Station pays 8,250 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1005.png",
     "image": "https://joycity.herokuapp.com/images/1005.png",
     "name": "Gas Station"
   },
-  "1007": {
+  "8": {
     "description": "Joy City Ride Wash pays 3,000 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1006.png",
     "image": "https://joycity.herokuapp.com/images/1006.png",
     "name": "Ride Wash"
   },
-  "1015": {
+  "14": {
     "description": "Joy City Ride Rental pays 2,100 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1007.png",
     "image": "https://joycity.herokuapp.com/images/1007.png",
     "name": "Ride Rental"
   },
-  "1016": {
+  "15": {
     "description": "Joy City Mechanic pays 3,750 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1008.png",
     "image": "https://joycity.herokuapp.com/images/1008.png",
     "name": "Mechanic"
   },
-  "1006": {
+  "7": {
     "description": "Joy City Power Station pays 6,500 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1009.png",
     "image": "https://joycity.herokuapp.com/images/1009.png",
     "name": "Power Station"
   },
-  "1008": {
+  "9": {
     "description": "Joy City Cinema pays 10,800 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1010.png",
     "image": "https://joycity.herokuapp.com/images/1010.png",
     "name": "Cinema"
   },
-  "1011": {
+  "10": {
     "description": "Joy City Restaurant pays 5,225 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1011.png",
     "image": "https://joycity.herokuapp.com/images/1011.png",
     "name": "Restaurant"
   },
-  "1012": {
+  "11": {
     "description": "Joy City Motel pays 13,125 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1012.png",
     "image": "https://joycity.herokuapp.com/images/1012.png",
     "name": "Motel"
   },
-  "1013": {
+  "12": {
     "description": "Joy City Supermarket pays 4,500 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1013.png",
     "image": "https://joycity.herokuapp.com/images/1013.png",
     "name": "Supermarket"
   },
-  "1014": {
+  "13": {
     "description": "Joy City Coffee House pays 1,500 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.herokuapp.com/images/1014.png",
     "image": "https://joycity.herokuapp.com/images/1014.png",
