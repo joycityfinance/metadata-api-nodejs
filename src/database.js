2265006a41044ff1dbe59a98ecdbe5de0be20043
midoelmelegy <midoelmelegy@gmail.com>
2021-12-10 20:22:21 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,90 +2,105 @@ module.exports = {
   "1000": {
     "description": "Joy City Ice Cream Mini pays 60 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/icecream/",
+    "animation_url": "https://joycity.finance/ar/token/1000/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1000.png",
     "name": "Ice Cream Mini"
   },
   "1001": {
     "description": "Joy City Grand Jeep pays 135 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/jeep/",
+    "animation_url": "https://joycity.finance/ar/token/1001/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1001.png",
     "name": "Grand Jeep"
   },
   "1002": {
     "description": "Joy City Mini Coop pays 375 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/mini/",
+    "animation_url": "https://joycity.finance/ar/token/1002/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1002.png",
     "name": "Mini Coop"
   },
   "1003": {
     "description": "Joy City Blue Rally pays 550 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/rally/",
+    "animation_url": "https://joycity.finance/ar/token/1003/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1003.png",
     "name": "Blue Rally"
   },
   "1004": {
     "description": "Joy City Black Sports pays 937.5 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/sports/",
+    "animation_url": "https://joycity.finance/ar/token/1004/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1004.png",
     "name": "Black Sports"
   },
   "1005": {
     "description": "Joy City Gas Station pays 8,250 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/gas/",
+    "animation_url": "https://joycity.finance/ar/token/1005/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1005.png",
     "name": "Gas Station"
   },
   "1007": {
     "description": "Joy City Car Wash pays 3,000 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/wash/",
+    "animation_url": "https://joycity.finance/ar/token/1006/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1006.png",
     "name": "Car Wash"
   },
   "1015": {
     "description": "Joy City Car Rental pays 2,100 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/rental/",
+    "animation_url": "https://joycity.finance/ar/token/1007/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1007.png",
     "name": "Car Rental"
   },
   "1016": {
     "description": "Joy City Car Repair pays 3,750 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/mechanic/",
+    "animation_url": "https://joycity.finance/ar/token/1008/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1008.png",
     "name": "Car Repair"
   },
   "1006": {
     "description": "Joy City Power Station pays 6,500 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/power/",
+    "animation_url": "https://joycity.finance/ar/token/1009/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1009.png",
     "name": "Power Station"
   },
   "1008": {
     "description": "Joy City Cinema pays 10,800 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/cinema/",
+    "animation_url": "https://joycity.finance/ar/token/1010/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1010.png",
     "name": "Cinema"
   },
   "1011": {
     "description": "Joy City Restaurant pays 5,225 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/restaurant/",
+    "animation_url": "https://joycity.finance/ar/token/1001/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1011.png",
     "name": "Restaurant"
   },
   "1012": {
     "description": "Joy City Motel pays 13,125 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/motel/",
+    "animation_url": "https://joycity.finance/ar/token/1012/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1012.png",
     "name": "Motel"
   },
   "1013": {
     "description": "Joy City Supermarket pays 4,500 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/market/",
+    "animation_url": "https://joycity.finance/ar/token/1013/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1013.png",
     "name": "Supermarket"
   },
   "1014": {
     "description": "Joy City Coffee House pays 1,500 JOY tokens weekly to NFT holder.",
     "external_url": "https://joycity.finance/buy/coffee/",
+    "animation_url": "https://joycity.finance/ar/token/1014/joycityGLB/asset1.glb",
     "image": "https://joycity.herokuapp.com/images/1014.png",
     "name": "Coffee House"
   }
